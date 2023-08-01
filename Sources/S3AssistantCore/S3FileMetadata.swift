4359dd70000ca6567e705321e3407983634c9ef4
Michael Redig <mredig@gmail.com>
2023-08-01 16:39:24 -0500

    (refactor) rename to s3 assistant

--- Sources/S3AssistantCore/S3FileMetadata.swift
+++ Sources/S3AssistantCore/S3FileMetadata.swift
@@ -53,39 +53,39 @@ public struct S3FileMetadata: Codable, CustomStringConvertible {
 			let keyNode = xmlNode.children?.first(where: { $0.name == "Key" }),
 			let keyValue = keyNode.stringValue,
 			keyValue.isEmpty == false
-		else { throw WasabiFileMetadataError.invalidXMLNode(reason: "Invalid Key Node", node: xmlNode) }
+		else { throw S3FileMetadataError.invalidXMLNode(reason: "Invalid Key Node", node: xmlNode) }
 		self.key = keyValue
 
 		guard
 			let lastModifiedNode = xmlNode.children?.first(where: { $0.name == "LastModified" }),
 			let lastModifiedValue = lastModifiedNode.stringValue,
 			let modifiedDate = Self.isoFormatter.date(from: lastModifiedValue)
-		else { throw WasabiFileMetadataError.invalidXMLNode(reason: "Invalid LastModified Node", node: xmlNode) }
+		else { throw S3FileMetadataError.invalidXMLNode(reason: "Invalid LastModified Node", node: xmlNode) }
 		self.lastModified = modifiedDate
 
 		guard
 			let eTagNode = xmlNode.children?.first(where: { $0.name == "ETag" }),
 			let eTagValue = eTagNode.stringValue,
 			eTagValue.isEmpty == false
-		else { throw WasabiFileMetadataError.invalidXMLNode(reason: "Invalid ETag Node", node: xmlNode) }
+		else { throw S3FileMetadataError.invalidXMLNode(reason: "Invalid ETag Node", node: xmlNode) }
 		self.eTag = eTagValue
 
 		guard
 			let sizeNode = xmlNode.children?.first(where: { $0.name == "Size" }),
 			let sizeValueStr = sizeNode.stringValue,
 			let sizeValue = Int(sizeValueStr)
-		else { throw WasabiFileMetadataError.invalidXMLNode(reason: "Invalid Size Node", node: xmlNode) }
+		else { throw S3FileMetadataError.invalidXMLNode(reason: "Invalid Size Node", node: xmlNode) }
 		self.size = sizeValue
 
 		guard
 			let storageClassNode = xmlNode.children?.first(where: { $0.name == "StorageClass" }),
 			let storageClassValue = storageClassNode.stringValue,
 			storageClassValue.isEmpty == false
-		else { throw WasabiFileMetadataError.invalidXMLNode(reason: "Invalid StorageClass Node", node: xmlNode) }
+		else { throw S3FileMetadataError.invalidXMLNode(reason: "Invalid StorageClass Node", node: xmlNode) }
 		self.storageClass = storageClassValue
 	}
 
-	enum WasabiFileMetadataError: Error {
+	enum S3FileMetadataError: Error {
 		case invalidXMLNode(reason: String, node: XMLNode)
 	}
 }
