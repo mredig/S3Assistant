2bc395f56b08c9315a090d15dc7d2d12823246f2
Michael Redig <mredig@gmail.com>
2023-08-03 00:52:21 -0500

    (nit) clean up some of the cmd line code

--- Sources/S3Assistant/S3Assistant.swift
+++ Sources/S3Assistant/S3Assistant.swift
@@ -17,12 +17,14 @@ struct S3Assistant: AsyncParsableCommand {
 			serviceURL: SwiftlyDotEnv["serviceURL"]!,
 			region: "\(SwiftlyDotEnv["region"]!)")
 
-		try await listAccumulatedFileInfo(on: controller)
+		try await listAccumulatedFileInfo(in: "logs", prefix: "plex-folder/", on: controller)
 //		try await getRecentFiles(on: controller)
 //		try await deleteOldFileLoop()
-//		try await getSizeOfWeddingFootage(on: controller)
 //		try await getFile(on: controller)
-//		try await moveFiles(on: controller)
+//		try await moveFiles(
+//			in: "logs",
+//			operation: .prefix(sourcePrefix: <#T##String#>, destinationPrefix: <#T##String#>, overwrite: <#T##Bool#>),
+//			on: <#T##S3Controller#>)
     }
 
 	func deleteOldFileLoop(on controller: S3Controller) async throws {
@@ -82,24 +84,29 @@ struct S3Assistant: AsyncParsableCommand {
 		return Array(oldFiles.prefix(10000))
 	}
 
-	func listAccumulatedFileInfo(on controller: S3Controller) async throws {
-		let results = try await controller
-			.listAllFiles(
-				in: "logs",
-				prefix: "plex-folder/plex-folder/",
-				delimiter: "/",
-				recurse: false)
+	func listAccumulatedFileInfo(
+		in bucket: String,
+		prefix: String?,
+		on controller: S3Controller) async throws {
+			let results = try await controller
+				.listAllFiles(
+					in: bucket,
+					prefix: prefix,
+					delimiter: "/",
+					recurse: false)
 
-		print(results.prettyPrinted)
-	}
+			print(results.prettyPrinted)
+		}
 
 	func getRecentFiles(on controller: S3Controller) async throws {
 
-		let oneDayAgo = Date().addingTimeInterval(-86400)
+		let oneDayAgo = Date().addingTimeInterval(-86400 * 5)
 
 		let files = try await controller.listAllFiles(
 			in: "logs",
+//			prefix: ,
 			delimiter: "/",
+			recurse: false,
 			filter: { result, _ in
 				let recentFiles = result.files.filter { $0.lastModified > oneDayAgo }
 				print("found \(recentFiles.count)...")
@@ -110,5 +117,49 @@ struct S3Assistant: AsyncParsableCommand {
 			.filter { $0.name.contains("plex") == false }
 			.sorted(by: { $0.lastModified < $1.lastModified } )
 			.forEach { print($0) }
+	}
+
+	func getSizeOfFolder(named folderName: String, on controller: S3Controller) async throws {
+		let sizeFormatter = ByteCountFormatter()
+		sizeFormatter.countStyle = .file
+
+		var totalSize: Int = 0
+		_ = try await controller
+			.listAllFiles(
+				in: ENV["bucket"]!,
+				prefix: folderName,
+				delimiter: "/",
+				pageLimit: nil,
+				recurse: true) { result, _ in
+					let thisSize = result.files.map(\.size).reduce(0, +)
+					print("added page size of \(sizeFormatter.string(fromByteCount: Int64(thisSize)))")
+					totalSize += thisSize
+					return result.files
+				}
+
+		print("Total size of directory: \(sizeFormatter.string(fromByteCount: Int64(totalSize)))")
+	}
+
+	/// not very useful since it just prints out the byte size, but decent proof of concept at least
+	func getFile(withKey key: String, on controller: S3Controller) async throws {
+		let data = try await controller
+			.getObject(
+				in: ENV["bucket"]!,
+				withKey: key)
+
+		print(data)
+	}
+
+	func moveFiles(
+		in bucket: String,
+		operation: S3Controller.WasabiMoveOperation,
+		on controller: S3Controller) async throws {
+			let data = try await controller
+				.wasabiRenameFiles(
+					in: bucket,
+					operation: operation)
+
+			let xml = try XMLDocument(data: data)
+			print(xml.xmlString(options: .nodePrettyPrint))
 	}
 }
