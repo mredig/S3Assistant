bd5d009039f1d94188436bce42ce049ed204c8a6
Michael Redig <mredig@gmail.com>
2024-10-12 00:46:17 -0500

    (feat) add s3 list object versions result

---
+++ Sources/S3AssistantCore/S3ListObjectVersionsResult.swift
@@ -0,0 +1,22 @@
+public struct S3ListObjectVersionsResult: CustomStringConvertible {
+	public let prefix: String?
+	public let delimiter: String?
+	public let nextMarker: (key: String, versionID: String)?
+
+	public let versions: [S3Object]
+
+	public var description: String {
+		var accum: [String] = []
+		if let prefix {
+			accum.append("S3 ListObjectVersionsResult: \(prefix)")
+		} else {
+			accum.append("S3 ListObjectVersionsResult:")
+		}
+		delimiter.map { accum.append("\tdelimiter: \($0)")}
+		nextMarker.map { accum.append("\tnextMarker: \($0)")}
+		accum.append("\tversions:")
+		accum.append(versions.map(\.description).map { $0.addIndentation(count: 2) }.joined(separator: "\n"))
+
+		return accum.joined(separator: "\n")
+	}
+}
