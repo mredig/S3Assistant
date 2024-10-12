4eb91955118345f80d31c5958e0428bb879bcd72
Michael Redig <mredig@gmail.com>
2024-10-12 04:22:25 -0500

    (refactor) remove unused array extension

--- Sources/S3AssistantCore/Array+S3Object.swift
+++ Sources/S3AssistantCore/Array+S3Object.swift
@@ -1,35 +1,5 @@
 import Foundation
 
-public extension Array where Element == S3Object {
-	func deleteList(quiet: Bool = false) throws -> XMLDocument {
-		guard count <= 1000 else { throw ArrayError.moreThan1000Objects }
-
-		let doc = XMLDocument(kind: .document)
-		let attribute = XMLNode(kind: .attribute)
-		attribute.name = "xmlns"
-		attribute.stringValue = "http://s3.amazonaws.com/doc/2006-03-01/"
-		let deleteElement = XMLElement(name: "Delete")
-		deleteElement.addAttribute(attribute)
-		doc.addChild(deleteElement)
-
-		let quietElement = XMLElement(name: "Quiet", stringValue: "\(quiet)")
-		deleteElement.addChild(quietElement)
-
-		for item in self {
-			let objectElement = XMLElement(name: "Object")
-			let	keyElement = XMLElement(name: "Key", stringValue: item.key)
-			objectElement.addChild(keyElement)
-			deleteElement.addChild(objectElement)
-		}
-
-		return doc
-	}
-
-	enum ArrayError: Error {
-		case moreThan1000Objects
-	}
-}
-
 public extension Array where Element: CustomStringConvertible {
 	var prettyPrinted: String {
 		"""
