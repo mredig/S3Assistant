a9a1f1046c1d69a78a1f959aeb71780b07255b71
Michael Redig <mredig@gmail.com>
2024-10-12 04:19:18 -0500

    (refactor) project org

--- Sources/S3AssistantCore/S3Responses/S3ListObjectVersionsResult.swift
+++ Sources/S3AssistantCore/S3Responses/S3ListObjectVersionsResult.swift
@@ -6,6 +6,24 @@ public struct S3ListObjectVersionsResult: Decodable, CustomStringConvertible {
 	public let deleteMarkers: [S3ObjectVersionDeleteMarker]
 	public let nextMarker: NextMarker?
 
+	public var content: [ContentOption] {
+		versions.map(ContentOption.version) + deleteMarkers.map(ContentOption.deleteMarker)
+	}
+
+	public enum ContentOption: S3ObjectIdentifierProvider {
+		case version(S3ObjectVersion)
+		case deleteMarker(S3ObjectVersionDeleteMarker)
+
+		public var objectIdentifier: S3ObjectIdentifier {
+			switch self {
+			case .version(let s3ObjectVersion):
+				s3ObjectVersion.objectIdentifier
+			case .deleteMarker(let s3ObjectVersionDeleteMarker):
+				s3ObjectVersionDeleteMarker.objectIdentifier
+			}
+		}
+	}
+
 	public init(
 		name: String,
 		prefix: String?,
@@ -27,8 +45,14 @@ public struct S3ListObjectVersionsResult: Decodable, CustomStringConvertible {
 		let name = try container.decode(String.self, forKey: .name)
 		let prefix = try container.decodeIfPresent(String.self, forKey: .prefix)
 		let delimiter = try container.decodeIfPresent(String.self, forKey: .delimiter)
-		let versions = try container.decode([S3ObjectVersion].self, forKey: .versions)
-		let deleteMarkers = try container.decode([S3ObjectVersionDeleteMarker].self, forKey: .deleteMarkers)
+		let versions = try container.decode([S3ObjectVersion].self, forKey: .versions).with { instance in
+			guard let delimiter else { return }
+			instance = instance.map { $0.withDelimiter(delimiter) }
+		}
+		let deleteMarkers = try container.decode([S3ObjectVersionDeleteMarker].self, forKey: .deleteMarkers).with { instance in
+			guard let delimiter else { return }
+			instance = instance.map { $0.withDelimiter(delimiter) }
+		}
 		let nextMarker = try? NextMarker(from: decoder)
 
 		self.init(
@@ -51,16 +75,20 @@ public struct S3ListObjectVersionsResult: Decodable, CustomStringConvertible {
 	}
 
 	public var description: String {
-		"""
-		S3ListVersionResult: \(name)
-			Prefix: \(prefix ?? "##noprefix##")
-			Delimiter: \(delimiter ?? "##no delimiter##")
-			Versions:
-		\(versions.map({"\($0)"}).joined(separator: "\n").addIndentation(count: 2))
-			DeleteMarkers:
-		\(deleteMarkers.map({"\($0)"}).joined(separator: "\n").addIndentation(count: 2))
+		var accum: [String] = []
+		if let prefix {
+			accum.append("S3 ListObjectVersionsResult: \(prefix)")
+		} else {
+			accum.append("S3 ListObjectVersionsResult:")
+		}
+		delimiter.map { accum.append("\tdelimiter: \($0)")}
+		nextMarker.map { accum.append("\tnextMarker: \($0)")}
+		accum.append("\tversions:")
+		accum.append(versions.map(\.description).map { $0.addIndentation(count: 2) }.joined(separator: "\n"))
+		accum.append("\tdelete markers:")
+		accum.append(deleteMarkers.map(\.description).map { $0.addIndentation(count: 2) }.joined(separator: "\n"))
 
-		"""
+		return accum.joined(separator: "\n")
 	}
 
 	enum CodingKeys: String, CodingKey {
