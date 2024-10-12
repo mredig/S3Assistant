5d79c7b9ced2dae41b8758c98e235c1a10bd0fa4
Michael Redig <mredig@gmail.com>
2024-10-12 01:18:20 -0500

    (refactor) debug output

--- Sources/S3AssistantCore/Array+S3Object.swift
+++ Sources/S3AssistantCore/Array+S3Object.swift
@@ -30,11 +30,21 @@ public extension Array where Element == S3Object {
 	}
 }
 
-public extension Array where Element == S3Object {
+public extension Array where Element: CustomStringConvertible {
 	var prettyPrinted: String {
 		"""
 		[
 		\(self.map(\.description).joined(separator: ",\n").addIndentation(count: 1))
+		]
+		"""
+	}
+}
+
+public extension Array where Element: CustomDebugStringConvertible {
+	var prettyDebugPrinted: String {
+		"""
+		[
+		\(self.map(\.debugDescription).joined(separator: ",\n").addIndentation(count: 1))
 		]
 		"""
 	}
